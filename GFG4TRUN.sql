d61dc788343d5b2d8bbd0bbc51e616135484285a
jyothikapolothu150-design <jyothikapolothu150@gmail.com>
2026-01-11 05:34:41 +0530

    DEL/TRUN

---
+++ GFG4TRUN.sql
@@ -0,0 +1,15 @@
+/*Drop means deleting what you have created ata time there is nothing to see
+Syntax:
+
+DROP object object_name ;*/
+ Drop TABLE subtable;
+ show tables in jyo_2026;;
+
+ DROP database database_name;
+
+
+ /* The TRUNCATE command is a Data Definition Language (DDL) action that removes all rows from a table but preserves the structure of the table for future use.
+ Syntax:
+ TRUNCATE TABLE  table_name;*/
+ TRUNCATE table  cust;
+ show tables in jyo_2026;;
